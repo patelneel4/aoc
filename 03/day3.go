f80e37adcec58345081cf48c5fe60dcbdbb64a93
Neel Patel <7669716+patelneel4@users.noreply.github.com>
2021-12-23 19:33:17 +1100

    Changed gamma and epsilon calc to generic most and least calc func to be reused for part 2

--- 03/day3.go
+++ 03/day3.go
@@ -5,36 +5,62 @@ import (
 	"fmt"
 	"strconv"
 )
-
-
-func Part1(lines []string) {
-	gamma, epsilon := "", ""
-	for i:=0; i <len(lines[0]); i++{
-		count0, count1 := 0,0
-		for _, line := range lines{
-			if line[i] == '0'{
-				count0++
-			} else {
-				count1++
-			}
-		}
-		if(count0 > count1){
-			gamma += "0"
-			epsilon += "1"
+func mostCommonAndLeastBit(lines [] string, i int) (string, string){
+	count0, count1 := 0,0
+	for _, line := range lines{
+		if line[i] == '0'{
+			count0++
 		} else {
-			gamma += "1"
-			epsilon += "0"
+			count1++
 		}
+	}
+	if(count0 > count1){
+		return "0", "1"
+	} else {
+		return "1", "0"
+	}
+
+}
+
+func part1(lines []string) {
+	gamma, epsilon := "",""
+	for i:=0; i <len(lines[0]); i++{
+		g, e :=  mostCommonAndLeastBit(lines, i)
+		gamma += g
+		epsilon += e
 	}
 	gammaInt, _ := strconv.ParseInt(gamma,2,64)
 	epsilonInt, _ := strconv.ParseInt(epsilon,2,64)
 	fmt.Println(int(gammaInt) * int(epsilonInt))
 }
-// func Part2(sc *bufio.Scanner) int{
-	
-// }
-
+func filteredLines(lines []string, i int, isCommon bool) string{
+	if len(lines) == 1{
+		return lines[0]
+	}
+	most, least := mostCommonAndLeastBit(lines, i)
+	comparer := ""
+	if isCommon {
+		comparer = most
+	} else {
+		comparer = least
+	}
+	filteredList := make([]string, 0)
+	for _, line := range lines {
+		if string(line[i]) == comparer {
+			filteredList = append(filteredList, line)
+		}
+	}
+	return filteredLines(filteredList, i+1, isCommon)
+}
+func part2(lines []string){
+	oxygen := filteredLines(lines, 0, true)
+	scrubber := filteredLines(lines, 0, false)
+	oxygenInt, _ := strconv.ParseInt(oxygen,2,64)
+	scrubberInt, _ := strconv.ParseInt(scrubber,2,64)
+	fmt.Println(int(oxygenInt) * int(scrubberInt))
+}
 func main() {
 	lines, _:= utils.ReadStringArrayFromFile("input.txt")
-	Part1(lines)
+	part1(lines)
+	part2(lines)
 }
